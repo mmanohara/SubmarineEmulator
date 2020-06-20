f145a1c61f3a0f5d17edfbf88980f1a4ecf4d37c
Tyler Colenbrander <tylercolenbrander@gmail.com>
2020-06-20 13:47:04 -0700

    Simplified code
    Simplified the noise function. Still have to write code dealing with location. Added .pyc files to .gitignore

--- Robotics_Simulator/noise_function.py
+++ Robotics_Simulator/noise_function.py
@@ -13,17 +13,12 @@ import waveform_generator_frequency_modulated as fm
 
 
 #Function to add noise to an input waveform
-def noise(input_waveform):
+def noise(input_wave,noise):
 
-    output_waveform = input_waveform
-    
-    #Goes through each waveform value in the input_waveform and adds noise
-    x = 0
-    for waveform in output_waveform:
-        output_waveform[x] = output_waveform[x] + np.random.normal(0,noise_variance)
-        x += 1
+    output_waveform = input_wave + np.random.normal(0,noise,size=len(input_wave))
     
     return output_waveform
+
 
 # Code testing region.
 if __name__ == '__main__':
@@ -33,7 +28,7 @@ if __name__ == '__main__':
     times, input_waveform = fm.wave_gen_FM([(10000, 1)], num_pts)
     noise_variance = 0.5
     
-    output_waveform = noise(input_waveform)
+    output_waveform = noise(input_waveform,noise_variance)
     plt.figure()
     plt.plot(times, output_waveform)
     #plt.plot(times, input_waveform)
