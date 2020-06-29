ba571f470b9baae4bce2781ee08b7c1f9b47c54d
chase <cblanche@caltech.edu>
2020-06-28 20:13:26 -0400

    gotta pull`

--- Robotics_Simulator/signal_angle_detect.py
+++ Robotics_Simulator/signal_angle_detect.py
@@ -8,12 +8,24 @@ Returns a vector representing the direction of origin of the pings
 
 """
 
+# This has not actually been done yet
+# TODO: Use Reuben's code to generate a waveform and determine the phases
+# from that
+# the waveform you can expect in from tyler is in the form of two one
+# dimensional arrays, one of times and one of voltages
+# I can assume that the ping will be more than 1 millisecond long
+# I know that for the most part 1 millisecond is an integer number of periods
+# But I can also use the known period based on the frequency to round the time
+# down to an integer multiple of the period to make my math actually work out
+
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Known constants
 SPEED_OF_SOUND = 1480 # meters per second
 HYDROPHONE_SPACING = 0.1 # meters
+# NOTE: Hydrophone spacing must be less than half of a wavelength
 
 # hydrophones are in a square, numbered 1-4 like the quadrants 
 # (phones 1 and 2 are on same x axis, 3 and 4 on same y)
@@ -40,19 +52,19 @@ def find_origin(c, freq, d, p1, p2, p3, p4):
         
     p1 : TYPE float (between 0 and 2pi)
         DESCRIPTION.
-        Phase of wave at hydrophone 1 (upper right)
+        Phase of wave at hydrophone 1 (upper left)
     
     p2 : TYPE float (between 0 and 2pi)
         DESCRIPTION.
-        Phase of wave at hydrophone 2 (upper left)
+        Phase of wave at hydrophone 2 (upper right)
         
     p3 : TYPE float (between 0 and 2pi)
         DESCRIPTION.
-        Phase of wave at hydrophone 3 (bottom left)
+        Phase of wave at hydrophone 3 (bottom right)
         
     p4 : TYPE float (between 0 and 2pi)
         DESCRIPTION.
-        Phase of wave at hydrophone 4 (bottom right)
+        Phase of wave at hydrophone 4 (bottom left)
 
     Returns
     -------
@@ -61,13 +73,17 @@ def find_origin(c, freq, d, p1, p2, p3, p4):
     '''
     
     # v_x, v_y, and v_z are the x and y components of the wave direction
-    v_x = c * (p1 - p2 - p3 + p4)/(4 * np.pi * d * freq)
-    v_y = (p1 + p2 - p3 - p4)/(4 * np.pi * d * freq)
-    v_z = sqrt(1 - pow(v_x, 2) - pow(v_y, 2))
+    v_x = c * (p2 - p1 - p4 + p3)/(4 * np.pi * d * freq)
+    v_y = (p2 + p1 - p4 - p3)/(4 * np.pi * d * freq)
+    v_z = math.sqrt(1 - pow(v_x, 2) - pow(v_y, 2))
     
     return np.array([v_x, v_y, v_z])
     
+def find_phase():
+    return
     
     
+if __name__ == "__main__": 
+    print("Executed when invoked directly")
     
     
