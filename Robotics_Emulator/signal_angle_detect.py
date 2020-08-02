73b6387861deb5f5573626b32edca02692fb7079
Reuben <52803246+ReubenRosenberg@users.noreply.github.com>
2020-08-01 19:07:43 -0500

    Added phase detection function. Function does not work. It uses the lowpass filter method.
    I don't know enough about what I'm doing with scipy.signal to properly troubleshoot this (and I also don't know how to make it analog). Maybe someone else can find where I've gone wrong.

--- Robotics_Emulator/signal_angle_detect.py
+++ Robotics_Emulator/signal_angle_detect.py
@@ -4,7 +4,7 @@ Created on Tue May 5 21:08:03 2020
 
 @author: chase
 
-Returns a vector representing the direction of origin of the pings 
+Returns a vector representing the direction of origin of the pings.
 
 """
 
@@ -18,16 +18,15 @@ Returns a vector representing the direction of origin of the pings
 # But I can also use the known period based on the frequency to round the time
 # down to an integer multiple of the period to make my math actually work out
 
-import math
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy import signal
 
 # Known constants
 SPEED_OF_SOUND = 1480 # meters per second
 HYDROPHONE_SPACING = 0.1 # meters
 # NOTE: Hydrophone spacing must be less than half of a wavelength
 
-# hydrophones are in a square, numbered 1-4 like the quadrants 
+# hydrophones are in a square, numbered 1-4 like the quadrants
 # (phones 1 and 2 are on same x axis, 3 and 4 on same y)
 
 def find_origin(c, freq, d, p):
@@ -41,15 +40,15 @@ def find_origin(c, freq, d, p):
         DESCRIPTION.
         Speed of the wave. This will be the speed of sound in water for most
         applications.
-        
+
     freq : TYPE float (preferably integer)
         DESCRIPTION.
         Frequency we are transmitting waves at.
-        
+
     d : TYPE float (preferably integer)
         DESCRIPTION.
         Distance between adjacent hydrophones (side length of square)
-        
+
     p : TYPE 1D array of floats (each between 0 and 2pi)
         DESCRIPTION.
         Phases of wave at each of the hydrophones
@@ -63,19 +62,19 @@ def find_origin(c, freq, d, p):
     Normalized 1D numpy array representing the direction of origin of the wave
 
     '''
-    
+
     # find phase differences
     # diffs[0] and [1] are x phase diffs on top and bottom, respectively
     # diffs[2] and [3] are y phase diffs on the left and right, respectively
     diffs = np.array([p[1]-p[0], p[2]-p[3], p[0]-p[3], p[1]-p[2]])
-    
+
     #trim phase differences to force between +/- pi
-    diffs = diffs % (2 * np.pi) 
-    
+    diffs = diffs % (2 * np.pi)
+
     for x in range(4):
         if diffs[x] > np.pi:
             diffs[x] = diffs[x] - (2 * np.pi)
-            
+
     # v_x, v_y, and v_z are the x, y, and z components of the wave direction
     v_x = c * (diffs[0] + diffs[1])/(4 * np.pi * d * freq)
     v_y = c * (diffs[2] + diffs[3])/(4 * np.pi * d * freq)
@@ -84,13 +83,13 @@ def find_origin(c, freq, d, p):
         v_z = 0
     else:
         v_z = np.sqrt(v_z)
-        
-    
+
+
     return np.array([v_x, v_y, v_z])
-    
+
 def find_phase(freq, times, waves):
     '''
-    Finds the phase of a given signal given its frequency and 1D arrays of 
+    Finds the phase of a given signal given its frequency and 1D arrays of
     times and amplitudes at those times
 
     Parameters
@@ -98,12 +97,12 @@ def find_phase(freq, times, waves):
     freq : TYPE float (preferably integer)
         DESCRIPTION.
         Frequency we are transmitting waves at.
-    
+
     times : TYPE 1D numpy array
         DESCRIPTION.
         List of times corresponding to each point in the waveform. Assumed to
         be ordered and in seconds.
-        
+
     waves : TYPE 1D numpy array
         DESCRIPTION.
         List of amplitudes comprising the final waveform
@@ -113,30 +112,90 @@ def find_phase(freq, times, waves):
     The phase difference of this particular wave
 
     '''
-    
+
     omega = 2 * np.pi * freq
-    
+
     # Trim this signal to the first ms, which is all we need
     times = times[times < times[0] + 0.001]
     if (times.size < 1):
         # If no measurements taken in the first millisecond, something wrong
         return 0; # what should I do?
-    
+
     # Trim the wave using the times as a guide
     waves = np.resize(waves, times.size)
-    
+
     # sums to find kI and kQ for Fourier inner product
     kI = np.sum(waves * np.cos(omega * times))
     kQ = np.sum(waves * np.sin(omega * times))
-    
+
     # Am I correct in using arctan2 here?
     phase = np.arctan2(kQ, kI)
 
     return phase
 
+
+def find_phase_2_electric_boogaloo(waveform, freq, cutoff,
+                                   order, sampling_freq):
+    """
+    Compute the phase of the input waveform relative to a fixed reference.
+
+    This function uses emulates the lowpass filtering method.
+
+    Parameters
+    ----------
+    waveform : 2-tuple of 1D numpy arrays
+        A representation of the input waveform. The first entry is a list of
+        times and the second entry is a list of corresponding amplitudes.
+
+    freq : float
+        The expected frequency of the incoming wave. All other frequencies are
+        filtered out.
+
+    cutoff : float
+        The cutoff frequency for the lowpass filter.
+
+    order : int
+        The order of the Butterworth lowpass filter.
+
+    sampling_freq : freq
+        The sampling frequency of the lowpass filter.
+
+    Returns
+    -------
+    float
+        The computed phase of the input waveform
+        (relative to a fixed internal reference).
+
+    """
+    # Multiply the input waveform by a cosinusoid at the expected frequency.
+    cos_prod = np.array(
+        [magnitude * np.cos(2*np.pi*freq*time)
+         for time, magnitude in zip(waveform[0], waveform[1])]
+    )
+
+    # Multiply the input waveform by a sinusoid at the expected frequency.
+    sin_prod = np.array(
+        [magnitude * np.sin(2*np.pi*freq*time)
+         for time, magnitude in zip(waveform[0], waveform[1])]
+    )
+
+    # Obtain parameters for a digit Butterworth lowpass filter with specified
+    # order and cutoff frequency.
+    sos = signal.butter(
+        order, 2 * cutoff / sampling_freq, btype='low',
+        analog=False, output='sos', fs=sampling_freq,
+    )
+
+    # Apply the Butterworth filter to the multiplied waveforms.
+    cos_prod_filt = signal.sosfiltfilt(sos, cos_prod)
+    sin_prod_filt = signal.sosfiltfilt(sos, sin_prod)
+
+    # By math, this gives the phase (in degrees).
+    return np.arctan(sin_prod_filt / cos_prod_filt)
+
 def signal_angle_detect(c, freq, d, times, waves):
     '''
-    Combines find_phase and find_origin to 
+    Combines find_phase and find_origin to
 
     Parameters
     ----------
@@ -144,23 +203,23 @@ def signal_angle_detect(c, freq, d, times, waves):
         DESCRIPTION.
         Speed of the wave. This will be the speed of sound in water for most
         applications.
-        
+
     freq : TYPE float (preferably integer)
         DESCRIPTION.
         Frequency we are transmitting waves at.
-        
+
     d : TYPE float (preferably integer)
         DESCRIPTION.
         Distance between adjacent hydrophones (side length of square)
-        
+
     times : list of four 1D numpy arrays
         DESCRIPTION.
-        List of four 1D numpy arrays representing the times of each 
+        List of four 1D numpy arrays representing the times of each
         measurement by each of the four hydrophones.
-    
+
     waves : list of four 1D numpy arrays
         DESCRIPTION.
-        List of four 1D numpy arrays representing the amplitudes of each 
+        List of four 1D numpy arrays representing the amplitudes of each
         measurement (corresponding to the times in times) by each of the four
         hydrophones.
 
@@ -173,10 +232,21 @@ def signal_angle_detect(c, freq, d, times, waves):
     phases = np.zeros(4)
     for i in range(0, 4):
         phases[i] = find_phase(freq, times[i], waves[i])
-        
+
     doa = find_origin(c, freq, d, phases)
-    
+
     return doa
-    
-#if __name__ == "__main__": 
-    # Executed when invoked directly
+
+
+if __name__ == "__main__":
+
+    # test code for phase_detection_2_electric_boogaloo
+    freq = 50000
+    cutoff = 4 * freq
+    order = 5
+    sampling_freq = 10 * freq
+    times = np.linspace(0, 1000e-3)
+    phase = 0
+    waveform = (times, np.cos(2*np.pi*(freq*times+phase)))
+    print(find_phase_2_electric_boogaloo(waveform, freq, cutoff,
+                                         order, sampling_freq))
