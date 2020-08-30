d42fdfa0520b8ecaeb06a8bfb175f28b8ddc7584
Mohith Manohara <mohith_manohara@yahoo.com>
2020-08-29 18:48:25 -0700

    Completed full integration of modulator and demodulator

--- Robotics_Emulator/entire_channel.py
+++ Robotics_Emulator/entire_channel.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from wave_channel import channel
 from transmit import transmit
-from phase_shift_checker import phase_shift_checker
+from phase_shift_checker import fourier_phase_shift_checker, phase_to_bit
 
 '''
 IMPORTANT NOTE: ALL UNITS ARE IN SI STANDARD UNITS. Thus, speed is in m/s,
@@ -21,31 +21,38 @@ frequency is in Hz, positions in m, etc.
 # Speed of sound in water
 SPEED_OF_SOUND = 1480
 
+def manual_demodulate(bitstream):
+    correct_bitstream = []
+    for i in range(1, len(bitstream)):
+        correct_bitstream.append(0 if bitstream[i] == bitstream[i - 1] else 1)
+    return np.array(correct_bitstream)
+
 # Code testing region.
 if __name__ == '__main__':
     
     # Set initial sub positions and velocities
     transmitter_position_initial = np.array([0, 0])
-    receiver_position_initial = np.array([SPEED_OF_SOUND/100, 0])
-    receiver_orientation_initial = np.array([1, 0])
-    transmitter_velocity = np.array([-1, 0])
-    receiver_velocity = np.array([0, 0])
-    receiver_spacing = 0.0254
+    receiver_position_initial = np.array([SPEED_OF_SOUND/10, 0])
+    receiver_orientation_initial = np.array([0, -1])
+    transmitter_velocity = np.array([1, 0])
+    receiver_velocity = np.array([-1, 0])
+    receiver_spacing = 0.02
     
     #Noise for transmission channel
-    noise_variance = 0
+    noise_variance = 0.01
     
     #Generate the bitstream
-    bitstream = np.random.randint(0,2,size=5)
+    bitstream = np.random.randint(0,2,size=100)
+    correct_bitstream = manual_demodulate(bitstream)
     print("Initial Bitsream:")
     print(bitstream)
     
     #Make a waveform from the bitstream
-    bit_rate = 2000
-    frequency = 10000
+    bit_rate = 200
+    frequency = 30000
     times, waveform = transmit(bitstream,bit_rate,encoding=None, 
-                               encoding_arg=0,modulation_type='PM', 
-                               relative_phase=180,frequency=frequency)
+                               encoding_arg=0,modulation_type='PSK', 
+                               PSK_phase=180,frequency=frequency)
     
     #Send the waveform through the noise channel
     output_times, output_waveforms = channel(times, waveform,
@@ -68,12 +75,21 @@ if __name__ == '__main__':
     print(phase_shift_bits)
     """
     
-    
+    error_list = [None] * 5
     for i in range(5):
-        recieved_bits_phase_shift[i] = phase_shift_checker(output_times[i], output_waveforms[i], 
-                                               time_int, frequency)
-    print("Phase Shifts of Output Waveforms:")
-    print(recieved_bits_phase_shift)
+        recieved_bits_phase_shift[i] = phase_to_bit(fourier_phase_shift_checker(output_times[i], output_waveforms[i], 
+                                               time_int, frequency))
+        if len(recieved_bits_phase_shift[i]) < len(correct_bitstream):
+            print(f"Lost {len(correct_bitstream)-len(recieved_bits_phase_shift[i])} bits")
+            error_list[i] = sum(np.logical_xor(recieved_bits_phase_shift[i], correct_bitstream[0:len(recieved_bits_phase_shift[i])]))
+        elif len(recieved_bits_phase_shift[i]) > len(correct_bitstream):
+            print(f"Extra {len(recieved_bits_phase_shift[i]) - len(correct_bitstream)} bits")
+            error_list[i] = sum(np.logical_xor(recieved_bits_phase_shift[i][0:len(correct_bitstream)], correct_bitstream))
+    #print("Phase Shifts of Output Waveforms:")
+    #print(recieved_bits_phase_shift)
+    print("Errors of output waveforms")
+    print(error_list)
+    
     #Plot the input waveform
     plt.figure()
     plt.plot(times, waveform, c='r', label="Input waveform")
