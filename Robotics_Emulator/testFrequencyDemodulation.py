19c8a41fc93637fc9bed754b1011ac49ee83676b
sonali <smadiset@caltech.edu>
2020-09-20 16:32:09 -0700

    Frequency Demodulator working, will comment later

--- Robotics_Emulator/testFrequencyDemodulation.py
+++ Robotics_Emulator/testFrequencyDemodulation.py
@@ -3,12 +3,13 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import wave_gen as wg
 
 #Constants
 SPEED_SOUND = 1480 # m / s 
 HYDRO_SPACING = 0.1 # m 
 
-def decodeFrequencyModulation(time, voltage, freq):
+def decodeFrequencyModulation(time, voltage, freq, delT):
     """
 
     Parameters
@@ -20,6 +21,7 @@ def decodeFrequencyModulation(time, voltage, freq):
         List of the voltages of the waveform which correspond to the given times.
     freq  :  1-D Numpy array
         List of possible frequencies which could correspond to the given data
+    bitPeriod : 
 
     Returns
     -------
@@ -28,39 +30,80 @@ def decodeFrequencyModulation(time, voltage, freq):
         time interval. 
 
     """
+    currentTime = delT / 2
+    delTPrime = delT / 4
+    bit = 0 
+    
+    time = time - time[0]
+    
+    index = time[time < currentTime - delTPrime].size
+    
     #Definition of Omega 
     omega = 2 * np.pi * freq     #Numpy array of all omega values 
     
-    #Trim the Signal Times, constrict to first millisecond
-    time = time[time < time[0] + 0.001]
+    frequencyList = []
     
-    #Check that signal exists
-    if (time.size < 1):
-        return 0
-    
-    #Trim the voltage given the constricted time 
-    voltage = np.resize(voltage, time.size)
-    
-    #Define Amplitudes
-    amplitude = np.zeros(omega.size)
-    
-    #find kI and kQ values for different frequencies 
-    for i in range(omega.size):
-        kI = np.sum(voltage * np.cos(omega[i] * time))
-        kQ = np.sum(voltage * np.sin(omega[i] * time))
-        #Find the Amplitude of the wave (Voltage value) at the given frequency
-        amplitude[i] = kI**2 + kQ**2 
+    while time[time > currentTime + delTPrime].size > 0:
+        measureTime = time[time >= currentTime - delTPrime]
         
-    #Compare the amplitudes - the highest amplitude corresponds with the frequency we want
-    maxAmplitude, givenFrequency = amplitude[0], freq[0]
-    
-    for i in range(amplitude.size):
-        if amplitude[i] > maxAmplitude:
-            maxAmplitude = amplitude[i]
-            givenFrequency = freq[i]   
-            
-    return givenFrequency
+        measureTime = measureTime[measureTime < currentTime - delTPrime + delT]
+        
+        indexJump = measureTime.size
+        
+        measureTime = measureTime[measureTime < currentTime + delTPrime]
+        
+        measureVoltage = np.take(voltage, range(index, index + measureTime.size))
+        
+        
+        amplitude = np.zeros(omega.size)
 
-#if __name__ == "__main__": 
-    #Insert 
+        for i in range(omega.size):
+            kI = np.sum(measureVoltage * np.cos(omega[i] * measureTime))
+            kQ = np.sum(measureVoltage * np.sin(omega[i] * measureTime))
+            
+            amplitude[i] = kI**2 + kQ**2
+        
+        
+        frequencyList.append(freq[np.argmax(amplitude)])
+        
+        currentTime += delT
+        index += indexJump
+        bit += 1
+    
+    return frequencyList
+
+if __name__ == "__main__": 
+    waves = []
+    
+    # works on 90, 180, 270 - [.5, 1, 1.5, 0]
+    waves.append([(0.001, 25000, 1, 0), (0.001, 25000, 1, 90),
+            (0.001, 20000, 1, 180), (0.001, 20000, 1, 270),
+            (0.001, 30000, 1, 0)])
+
+    # works with arbitrary lengths of 0s - [0, 0, 0, 0, 0, 0, 0]
+    waves.append([(0.004, 20000, 1, 0), (0.003, 20000, 1, 0), 
+                  (0.001, 20000, 1, 0)])
+    
+    # works with phase shifts then holds - [.05, 0, 0 ,1]
+    waves.append([(0.001, 25000, 1, 0), (0.003, 35000, 1, 90), 
+                  (0.001, 30000, 1, 180)])
+    
+    # pushes to nearest phase- [.5, .5, 1, 1, 1.5]
+    waves.append([(0.001, 24988, 1, 0), (0.001, 30021, 1, 95), 
+                  (0.001, 20030, 1, 85), (0.001, 35041, 1, 190), 
+                  (0.001, 34950, 1, 173), (0.001, 19949, 1, 274)])
+    
+    for wave in waves:
+        times, waveform = wg.wave_gen(wave)
+        #print(fourier_phase_shift_checker(times, waveform, .001, 20000))
+        print(decodeFrequencyModulation(times, waveform, np.array([20000, 25000, 30000, 35000]), 0.001))
+    
+    """# works on an amplitude that is not 1, as long as amp is constant
+    # it could work even if amp isnt constant, but mostly due to the error 
+    # margins I allow and I wouldn't push it
+    # [.5, 1, 1.5]
+    wave = ([(0.001, 20000, 2, 0), (0.001, 20000, 2, 90),
+            (0.001, 20000, 2, 180), (0.001, 20000, 2, 270)])
+    times, waveform = wg.wave_gen(wave)
+    print(phase_shift_checker(times, waveform, .001, 20000, amp = 2))"""
     
